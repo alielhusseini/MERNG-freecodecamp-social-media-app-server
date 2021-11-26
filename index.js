c46929b579c4e617a14d7228ccffdc757dacc507
Ali El Husseini <alielhusseini@Alis-MacBook-Air.local>
2021-11-26 23:22:48 +0200

    server side

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 // imports
 const express = require('express')
+const { ApolloServer } = require('apollo-server') // to create the server, will not use the express
+const gql = require('graphql-tag')
 const mongoose = require('mongoose')
 const cors = require('cors')
 require('dotenv/config')
 
 // setup
-const PORT = process.env.PORT || 5000
-
 const app = express()
-mongoose.connect(process.env.CONNECTION_URL)
-    .then(() => app.listen(PORT, () => console.log(`server connected to port ${PORT}`)))
-
 app.use(express.json())
 app.use(cors())
+
+const typeDefs = gql`
+    type Query{
+        sayHi: String!
+    }
+`
+const resolvers = {
+    Query: {
+        sayHi: () => 'hello world'
+    }
+}
+
+const port = process.env.PORT || 5000
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+server.listen({ port })
+    .then(res => console.log(`server running at ${res.url}`))
